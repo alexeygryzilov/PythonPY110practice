acdc1fdef6c54108636fb48112db5c6e0bbf05e5
Alexey <alexeygryzilov@gmail.com>
2024-01-02 19:52:18 +0300

    Lab 6

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import wishlist_view
+from store.views import products_page_view
 #  TODO Импортируйте ваше представление
 
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('wishlist/', wishlist_view, name="wishlist_view"),  # TODO Зарегистрируйте обработчик
+    path('wishlist/', wishlist_view, name="wishlist_view"),
+    path('wishlist/products/<slug:page>.html', products_page_view, name="products_page_view"),
+    path('wishlist/products/<int:page>', products_page_view),
 ]
