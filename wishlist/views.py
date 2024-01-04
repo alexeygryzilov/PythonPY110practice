78ad7f1ae45dd1fd02332f010371b31a57ee82dd
Alexey <alexeygryzilov@gmail.com>
2024-01-04 12:59:03 +0300

    Lab 6

--- wishlist/views.py
+++ wishlist/views.py
@@ -49,7 +49,7 @@ def wishlist_add_json(request, id_product: str):
         return JsonResponse({"answer": "Неудачное добавление в избранное"}, status=404,
                                 json_dumps_params={'ensure_ascii': False}) # TODO верните JsonResponse с ключом "answer" и значением "Неудачное добавление в избранное" и параметром status=404
 
-@login_required(login_url='login:login_view')
+#@login_required(login_url='login:login_view')
 def wishlist_del_json(request, id_product: str):
     """
     Удаление продукта из избранного и возвращение информации об успехе или неудаче в JSON
@@ -78,7 +78,7 @@ def wishlist_json(request):
         return JsonResponse({"answer": "пользователь не авторизован"}, status=404,
                                 json_dumps_params={'ensure_ascii': False})   # TODO верните JsonResponse с ключом "answer" и значением "Пользователь не авторизирован" и параметром status=404
 
-@login_required(login_url='login:login_view')
+#@login_required(login_url='login:login_view')
 def wishlist_remove_view(request, id_product):
     if request.method == "GET":
         result = remove_from_wishlist(request, id_product)  # TODO Вызвать функцию удаления из корзины
